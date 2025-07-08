dfc0e6cb478e65dd6bd4355e2de127278c3d3d0c
golzi99 <golzi99@gmail.com>
2025-07-08 20:41:10 +0300

    Updates

---
+++ _next/static/chunks/app/layout-d773900fa6b859ce.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{5683:()=>{},7680:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,5683,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[128,803,655,358],()=>s(7680)),_N_E=e.O()}]);
